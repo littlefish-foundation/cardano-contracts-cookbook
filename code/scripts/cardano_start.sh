9f40103f5f54b6188c12b04e4d1f4f761e5fbf22
albert-kevin <beire_@hotmail.com>
2023-01-30 16:02:29 +0100

    make the startscript more compact and simple

--- code/scripts/cardano_start.sh
+++ code/scripts/cardano_start.sh
@@ -4,13 +4,13 @@ tmux new-session -s ses-0 -n cardano-start -d
 # split window horizontally to panels 0 and 1
 tmux split-window -h
 # split panel 1 horizontally to panels 1 and 2 (left to right)
-tmux split-window -v -t 1
+tmux split-window -v -t 0
 
 # run commands in each pane
 tmux send-keys -t 0 'cd ~/cardano' Enter
 tmux send-keys -t 0 'cardano-node run \
    --topology topology.json \
-   --database-path ~/cardano/db/ \
+   --database-path db/ \
    --socket-path /tmp/node.socket \
    --host-addr 0.0.0.0 \
    --config config.json' Enter
@@ -21,13 +21,13 @@ tmux send-keys -t 1 'cardano-wallet serve \
    --port 8090 \
    --node-socket /tmp/node.socket \
    --testnet byron-genesis.json \
-   --database ~/cardano/wallet-db \
+   --database wallet-db \
    --token-metadata-server https://metadata.cardano-testnet.iohkdev.io' Enter
    
 tmux send-keys -t 2 "set -o vi" Enter
 tmux send-keys -t 2 "htop" Enter
 
-# activate pane 2, bottom right
+# activate pane 1, bottom right
 tmux select-pane -t 2
 
 # compile the end-result on screen
