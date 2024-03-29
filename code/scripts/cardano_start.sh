deafc7dc53dcc1519dd77c849c9c4b5c67e7412b
albert-kevin <beire_@hotmail.com>
2024-03-29 15:33:14 +0100

    add an extra window in tmux for a Flask webserver

--- code/scripts/cardano_start.sh
+++ code/scripts/cardano_start.sh
@@ -5,6 +5,8 @@ tmux new-session -s ses-0 -n cardano-start -d
 tmux split-window -h
 # split panel 1 horizontally to panels 1 and 2 (left to right)
 tmux split-window -v -t 0
+# split panel 2 vertically to create panels 2 and 3 (top to bottom)
+tmux split-window -v -t 2
 
 # run commands in each pane
 tmux send-keys -t 0 'cd ~/cardano' Enter
@@ -26,6 +28,11 @@ tmux send-keys -t 1 'cardano-wallet serve \
    
 tmux send-keys -t 2 "set -o vi" Enter
 tmux send-keys -t 2 "htop" Enter
+
+tmux resize-pane -t 3 -D 5
+tmux send-keys -t 3 "set -o vi" Enter
+tmux send-keys -t 3 'cd $(dirname $(find ~ -type f -name "cardano_start.sh" 2>/dev/null | head -n 1))' Enter
+# tmux send-keys -t 3 'gunicorn flaskserver:app -b 0.0.0.0:8050' Enter
 
 # activate pane 1, bottom right
 tmux select-pane -t 2
